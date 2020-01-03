0535749967c136b8fd61eb5a83ac656cbd319896
Ricardo Persaud <ricardo39985@gmail.com>
2020-01-03 01:34:45 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,7 +2,7 @@ class Owner
   # code goes here
   @@all = []
   attr_accessor :name, :species
-  def initialize(*name, species)
+  def initialize(name = "", species)
     @species = species
     if name
       @name= name
