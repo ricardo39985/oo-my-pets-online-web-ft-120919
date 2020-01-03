47b9a504392c1de1c9ad7b6e08de25ddbc80b5c8
Ricardo Persaud <ricardo39985@gmail.com>
2020-01-03 14:48:59 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,14 +2,11 @@ class Owner
   # code goes here
   @@all = []
 
-  attr_reader :species
+  attr_reader :species, :name
   def initialize(name)
     @name = name
     @species = 'human'
     @@all << self
-  end
-  def name
-    @name
   end
   def self.all
     @@all
